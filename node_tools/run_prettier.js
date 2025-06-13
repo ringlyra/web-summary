d200cfde5564e22bf9e1f93832115ac048cbba6f
Lyra <hamanaka.destiny@gmail.com>
2025-06-13 18:11:52 +0900

    Update and rename run_prettier.js to lint-summary.js

--- node_tools/run_prettier.js
+++
@@ -1,6 +0,0 @@
-const { execSync } = require("child_process");
-
-const summariesGlob = 'Summary/**/*.md';
-const cmd = `npx prettier --write '${summariesGlob}'`;
-console.log(cmd);
-execSync(cmd, { stdio: "inherit", shell: true });
